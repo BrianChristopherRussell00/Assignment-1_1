cbd60af14ad5fbaf106868c86a1144dad0588b40
Brian Russell <Brianrussell2017@yahoo.com>
2024-11-05 14:34:26 -0500

    Added assignments 1_1_2,3,4

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualBasic;
 
 namespace Assignment_1_1
-{
+{//Assignment 1-1
     internal class Program
     {
         static void Main(string[] args)
@@ -23,12 +23,37 @@ namespace Assignment_1_1
 
             string myAddress = Console.ReadLine();
 
-            Console.WriteLine("Hello " + myName + " . " + " you are " + myAge + " years old. " + "and you live at" + myAddress);
+            Console.WriteLine("Hello " + myName + " . " + " you are " + myAge + " years old. " + "and you live at " + myAddress);
             Console.WriteLine(mynumber);
             Console.WriteLine(myDoubleNum);
-            Console.WriteLine(mynumber+myDoubleNum);
-            Console.WriteLine(myDoubleNum/mynumber);
+            Console.WriteLine(mynumber + myDoubleNum);
+            Console.WriteLine(myDoubleNum / mynumber);
 
+            //Assignment 1-2
+            int priceOfFries = 2;
+            float priceOfBurger = 5;
+            double priceOfDrink = 2;
+            decimal burgerDeluxe = 6.99m;
+
+            int leastExpensiveItem = (int)Math.Min(priceOfFries, priceOfBurger);
+            int mostExpensiveItem = (int)Math.Min(priceOfDrink, priceOfBurger);
+
+
+            Console.WriteLine(mostExpensiveItem);
+            Console.WriteLine(leastExpensiveItem);
+
+            //Assignment 1-3,4
+            int newNumber;
+            int otherNewNumber;
+
+            Console.WriteLine("Give me a number:");
+            newNumber=Convert.ToInt32(Console.ReadLine());
+        
+            Console.WriteLine("Give me another number:");
+            otherNewNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The total of these numbers equals: " + (newNumber+otherNewNumber) );
+            Console.WriteLine("If you divide these two numbers you get: " + (newNumber+ otherNewNumber) );
         }
     }
 }
